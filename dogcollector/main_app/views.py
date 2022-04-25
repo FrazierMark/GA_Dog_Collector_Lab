59ef151d23ef19d34cecceb722231b0a28c4abdf
FrazierMark <m.mark.frazier@gmail.com>
2022-04-25 09:55:18 -0700

    Adding updating models

--- dogcollector/main_app/views.py
+++ dogcollector/main_app/views.py
@@ -4,20 +4,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-# Add the Dog class & list and view function below the imports
-class Dog:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-
-dogs = [
-    Dog('Lolo', 'beagle', 'foul little demon', 3),
-    Dog('Sachi', 'boxer', 'diluted tortoise shell', 0),
-    Dog('Raven', 'gloden doodle', '3 legged dog', 4)
-]
 
 
 def home(request):
