611d23f18ad56fb41c7d9ae1e8c3f5a49636652e
FrazierMark <m.mark.frazier@gmail.com>
2022-04-26 11:19:42 -0700

    Add edit and delete option for dogs

--- dogcollector/main_app/views.py
+++ dogcollector/main_app/views.py
@@ -6,12 +6,25 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.http import HttpResponse
 from .models import Dog
 
+# the template the CreateView and the UpdateView use is the same
+# templates/<app_name>/<model>_form.html
+# templates/main_app/dog_form.html
 
 
 class DogCreate(CreateView):
     model = Dog
     fields = '__all__'
+
+
+class DogUpdate(UpdateView):
+    model = Dog
+    fields = ['breed', 'description', 'age']
+
+
+class DogDelete(DeleteView):
+    model = Dog
     success_url = '/dogs/'
+
 
 def home(request):
     return HttpResponse('<h1>Hello World!</>')
@@ -25,6 +38,8 @@ def dogs_index(request):
     dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', {'dogs': dogs})
 
-def dogs_detail(request, dog_id): # (from urls) path('cats/<int:cat_id>/' <-- this is where cat_id comes from
+
+# (from urls) path('cats/<int:cat_id>/' <-- this is where cat_id comes from
+def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
     return render(request, 'dogs/detail.html', {'dog': dog})
